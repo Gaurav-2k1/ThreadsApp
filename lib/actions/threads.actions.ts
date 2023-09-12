cf9f05f57bbe0bb305907510e3f3ba710ef00bb3
Gaurav-2k1 <gauravbhukte1507@gmail.com>
2023-09-12 11:20:13 +0530

    first commit

---
+++ lib/actions/threads.actions.ts
@@ -0,0 +1,144 @@
+"use server";
+import { revalidatePath } from "next/cache";
+import Thread from "../models/thread.model";
+import User from "../models/user.model";
+import { connectToDB } from "../mongoose";
+import Community from "../models/community.model";
+
+interface Params {
+  text: string;
+  author: string;
+  communityId: string | null;
+  path: string;
+}
+
+export async function createThread({
+  text,
+  author,
+  communityId,
+  path,
+}: Params) {
+  try {
+    connectToDB();
+    const createThread = await Thread.create({
+      text,
+      author,
+      community: null,
+    });
+    await User.findByIdAndUpdate(author, {
+      $push: { threads: createThread._id },
+    });
+
+    revalidatePath(path);
+  } catch (err: any) {
+    throw new Error(`unavle to create thread :${err.message}`);
+  }
+}
+
+export async function fecthPosts(pageNumber = 1, pageSize = 20) {
+  try {
+    connectToDB();
+    const skipAmount = (pageNumber - 1) * pageSize;
+    const postQuery = Thread.find({
+      parentId: { $in: [null, undefined] },
+    })
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate({ path: "author", model: User })
+      .populate({
+        path: "community",
+        model: Community,
+      })
+      .populate({
+        path: "children", // Populate the children field
+        populate: {
+          path: "author", // Populate the author field within children
+          model: User,
+          select: "_id name parentId image", // Select only _id and username fields of the author
+        },
+      });
+    const totalPostCount = await Thread.countDocuments({
+      parentId: {
+        $in: [null, undefined],
+      },
+    });
+    const posts = await postQuery.exec();
+
+    const isNext = totalPostCount > skipAmount + posts.length;
+    return { posts, isNext };
+  } catch (err) {}
+}
+
+export async function fetchThreadById(threadId: string) {
+  connectToDB();
+
+  try {
+    const thread = await Thread.findById(threadId)
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id id name image",
+      })
+      .populate({
+        path: "community",
+        model: Community,
+        select: "_id id name image",
+      })
+      .populate({
+        path: "children", // Populate the children field
+        populate: [
+          {
+            path: "author", // Populate the author field within children
+            model: User,
+            select: "_id id name parentId image", // Select only _id and username fields of the author
+          },
+          {
+            path: "children", // Populate the children field within children
+            model: Thread, // The model of the nested children (assuming it's the same "Thread" model)
+            populate: {
+              path: "author", // Populate the author field within nested children
+              model: User,
+              select: "_id id name parentId image", // Select only _id and username fields of the author
+            },
+          },
+        ],
+      })
+      .exec();
+
+    return thread;
+  } catch (err) {
+    console.error("Error while fetching thread:", err);
+    throw new Error("Unable to fetch thread");
+  }
+}
+
+export async function addCommentToThread(
+  threadId: string,
+  commentText: string,
+  userId: string,
+  path: string
+) {
+  connectToDB();
+  try {
+    const originalThread = await Thread.findById(threadId);
+    if (!originalThread) {
+      throw new Error("Thread not found");
+    }
+
+    const commentThread = new Thread({
+      text: commentText,
+      author: userId,
+      parentId: threadId,
+    });
+    const savedCommentThread = await commentThread.save();
+
+    originalThread.children.push(savedCommentThread._id);
+    await originalThread.save();
+
+    revalidatePath(path);
+  } catch (err) {
+    console.error("Error while fetching thread:", err);
+    throw new Error("Unable to fetch thread");
+  }
+}
